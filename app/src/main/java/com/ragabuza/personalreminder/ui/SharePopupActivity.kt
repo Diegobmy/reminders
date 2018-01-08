c1016275eb5b82803bc5489e76d84fec3ae90517
Diego <diegobmoyses@gmail.com>
2018-01-08 03:26:44 -0200

    share popup new

--- app/src/main/java/com/ragabuza/personalreminder/ui/SharePopupActivity.kt
+++ app/src/main/java/com/ragabuza/personalreminder/ui/SharePopupActivity.kt
@@ -1,15 +1,15 @@
 package com.ragabuza.personalreminder.ui
 
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.ragabuza.personalreminder.R
-import kotlinx.android.synthetic.main.activity_link.*
-import android.content.DialogInterface
-import android.os.Build
-import android.support.v7.app.AlertDialog
-import android.view.Window
-import android.view.WindowManager
+import kotlinx.android.synthetic.main.popup_share.*
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ImageSpan
+import android.text.style.StyleSpan
 
 
 /**
@@ -19,15 +19,37 @@ class SharePopupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_link)
+        setContentView(R.layout.popup_share)
 
 
-        val urlString = "${intent.getStringExtra(Intent.EXTRA_SUBJECT)} \n\n ${intent.getStringExtra(Intent.EXTRA_TEXT)}"
-        tvLink.text = urlString
+        val imageSpan = ImageSpan(this, R.drawable.ic_link_white)
+
+        var urlString: SpannableString
+        val regex = Regex("https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)")
+
+        if (intent.getStringExtra(Intent.EXTRA_TEXT).toString().matches(regex)) {
+            urlString = SpannableString("${intent.getStringExtra(Intent.EXTRA_SUBJECT)}\n\nL ${intent.getStringExtra(Intent.EXTRA_TEXT)}")
+            urlString.setSpan(imageSpan, intent.getStringExtra(Intent.EXTRA_SUBJECT).length+2,intent.getStringExtra(Intent.EXTRA_SUBJECT).length+3,0)
+            urlString.setSpan(StyleSpan(Typeface.ITALIC), intent.getStringExtra(Intent.EXTRA_SUBJECT).length+2, urlString.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+        else
+            urlString = SpannableString(intent.getStringExtra(Intent.EXTRA_TEXT))
+
+        tvLabel.text = urlString
+        ivOutClick.setOnClickListener {
+            finish()
+        }
+
+        safe.setOnClickListener {  }
 
     }
 
     override fun onBackPressed() {
+        finish()
+    }
+
+    override fun onPause() {
+        super.onPause()
         finish()
     }
 }
